75afadf5f5dfc8ce854fdac88e94a406563ed302
Florin Asavei <asaveiflorin@gmail.com>
2018-12-08 00:27:44 +0200

    reset step 1 to zero

--- helloWorld/script/app.js
+++ helloWorld/script/app.js
@@ -3,20 +3,3 @@ console.log("js loaded");
 loadHeader();
 loadFooter();
 insertFavicon();
-
-var allImages = document.querySelectorAll("img");
-//clasic for loop
-for (var i = 0; i < allImages.length; i++) {
-  console.log(allImages[i]);
-}
-
-var allCatImages = document.querySelectorAll("#cat-container img");
-//ES6 syntax
-allCatImages.forEach(catImage => {
-  console.log(catImage)
-});
-
-var allDogImages = document.querySelectorAll("#dog-container img");
-allDogImages.forEach(dogImage => {
- console.log(dogImage)
-});
--- project/script/app.js
+++ project/script/app.js
@@ -7,39 +7,5 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function init() {
-  var getJokeButton = document.querySelector("#getJoke");
-
-  function chuckNorrisModel(fName, lName) {
-    this.firstName = fName || "Chuck";
-    this.lastName = lName || "Norris";
-  }
-
-  //event listeners
-  getJokeButton.addEventListener("click", getJokeFromServer);
-
-  function getJokeFromServer() {
-    var firstNameVal = document.querySelector("#firstName").value;
-    var lastNameVal = document.querySelector("#lastName").value;
-
-    var isSafeForWork = document.querySelector("#filderMature").checked;
-    var isOnlyNerdy = document.querySelector("#onlyNerdy").checked;
-
-    var myChuckNorris = new chuckNorrisModel(firstNameVal, lastNameVal);
-
-
-    var exludedCathegories = isSafeForWork ? "[explicit]" : null;
-
-    var params = {
-      firstName: myChuckNorris.firstName,
-      lastName: myChuckNorris.lastName,
-      exclude: exludedCathegories
-    };
-
-    if (isOnlyNerdy) {
-      params.limitTo = "[nerdy]";
-    }
-
-    console.log('params', params);
-
-  }
+  
 }
